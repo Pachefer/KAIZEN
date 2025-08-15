2f71bf27bd5babeabd0b44b166ca28ef1cd54019
Pachefer <67516534+Pachefer@users.noreply.github.com>
2025-08-16 03:01:57 +0800

    ai book

---
+++ ai/packages/openai/src/openai-tools.ts
@@ -0,0 +1,9 @@
+import { codeInterpreter } from './tool/code-interpreter';
+import { fileSearch } from './tool/file-search';
+import { webSearchPreview } from './tool/web-search-preview';
+
+export const openaiTools = {
+  codeInterpreter,
+  fileSearch,
+  webSearchPreview,
+};
